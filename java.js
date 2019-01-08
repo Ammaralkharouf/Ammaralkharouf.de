29feac59ff7d46478964683ec686e6182c6cdc5b
Ammaralkharouf <45965786+Ammaralkharouf@users.noreply.github.com>
2019-01-08 15:39:22 +0100

    initial

---
+++ java.js
@@ -0,0 +1,40 @@
+function openNav(){
+
+    document.getElementById('nav').style.width = "100%";
+}
+
+function closeNav(){
+
+    document.getElementById('nav').style.width = "0%";
+}
+
+function openNav(){
+
+    document.getElementById('nav').style.height = "100%";
+}
+
+function closeNav(){
+
+    document.getElementById('nav').style.height = "0%";
+}
+
+
+function openVan(){
+
+    document.getElementById('van').style.width = "100%";
+}
+
+function closeVan(){
+
+    document.getElementById('van').style.width = "0%";
+}
+
+function openVan(){
+
+    document.getElementById('van').style.height = "100%";
+}
+
+function closeVan(){
+
+    document.getElementById('van').style.height = "0%";
+}
